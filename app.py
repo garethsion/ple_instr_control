9e6ef6e988d48d5f82794289b0631f860a28d383
garethsion <garethsion@googlemail.com>
2022-09-04 10:47:58 +0100

    Think I worked out how to plot the oscope trace

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, config, render_template, request
-import plotly
-import plotly.express as px
-
-app = Flask(__name__)
-
-@app.route('/callback', methods=['POST', 'GET'])
-def cb():
-    return gm(request.args.get('data'))
--- appDash.py
+++ appDash.py
@@ -1,17 +1,29 @@
+import numpy as np
 import dash
 from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.long_callback import DiskcacheLongCallbackManager
+import diskcache
+
 import plotly
-import random
 import plotly.graph_objs as go
 from collections import deque
+import time
 
 from src.high_finesse_ws6.src.high_finesse import WavelengthMeter
+from src.Oscope import Oscope
 
 # Connect the wavemeter
 wlm = WavelengthMeter(dllpath="C:\\Program Files (x86)\\HighFinesse\\Wavelength Meter WS Ultimate 1653\\Projects\\64\\wlmData.dll")
+
+# Connect the oscilloscope
+resource_string = 'USB0::0x0AAD::0x010F::100899::INSTR'
+optstr = "AddTermCharToWriteBinBLock=True, TerminationCharacter='\n',AssureWriteWithTermChar=True, WriteDelay=20, ReadDelay=5"
+
+oscope = Oscope(resource_string, optstr)
+# oscope.setup_trace(channel='CHAN2', time_scale=0.05, volt_scale=0.08, pos=0)
 
 Xwlen = deque(maxlen=600)
 Xwlen.append(1)
@@ -25,7 +37,15 @@ Xpwr.append(1)
 Ypwr = deque(maxlen=600)
 Ypwr.append(wlm.powers[0])
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+oscope.setup_trace(channel='CHAN2', time_scale=0.05, volt_scale=0.08, pos=0)
+# trace = oscope.get_trace(channel='CHAN2', plotting=False)
+
+# Xtrace = np.linspace(0,len(trace), len(trace))
+# Ytrace = trace
+
+cache = diskcache.Cache('./cache')
+lcm = DiskcacheLongCallbackManager(cache)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG], long_callback_manager=lcm)
 
 app.layout = html.Div(
     [
@@ -44,24 +64,25 @@ app.layout = html.Div(
                     animate=False),
                 dcc.Interval(
                     id='graph-update-2',
-                    interval = 20,
-                    n_intervals = 0
+                    interval=20,
+                    n_intervals=0
             )]), width=4),
             dbc.Col(html.Div([
                 dcc.Graph(id='live-graph-3',
                     animate=False),
                 dcc.Interval(
                     id='graph-update-3',
-                    interval=20,
+                    interval=6000,
                     n_intervals=0
             )]), width=4)
         ])
     ]
 )
 
+
 @app.callback(
     Output('live-graph-1', 'figure'),
-    [Input('graph-update-1', 'n_intervals') ]
+    [Input('graph-update-1', 'n_intervals')],
 )
 def update_wavemeter(n):
     Xwlen.append(Xwlen[-1]+1)
@@ -75,7 +96,7 @@ def update_wavemeter(n):
     )
 
     return {'data': [data],
-            'layout' : go.Layout(xaxis=dict(range=[min(Xwlen),max(Xwlen)]),
+            'layout': go.Layout(xaxis=dict(range=[min(Xwlen),max(Xwlen)]),
                                  yaxis=dict(range=[min(Ywlen),max(Ywlen)]),
                                  title='Wavemeter', xaxis_title='', yaxis_title='Wavelength (nm)',
                                  font=dict(color='white'),
@@ -83,9 +104,10 @@ def update_wavemeter(n):
                                  plot_bgcolor='rgba(0,0,0,0)'
                     )}
 
+
 @app.callback(
     Output('live-graph-2', 'figure'),
-    [ Input('graph-update-2', 'n_intervals') ]
+    [Input('graph-update-2', 'n_intervals') ]
 )
 def update_wavepower(n):
     Xpwr.append(Xwlen[-1]+1)
@@ -95,11 +117,11 @@ def update_wavepower(n):
             x=list(Xpwr),
             y=list(Ypwr),
             name='Scatter',
-            mode= 'lines+markers'
+            mode='lines+markers'
     )
 
     return {'data': [data],
-            'layout' : go.Layout(xaxis=dict(range=[min(Xpwr),max(Xpwr)]),
+            'layout': go.Layout(xaxis=dict(range=[min(Xpwr),max(Xpwr)]),
                                  yaxis=dict(range=[min(Ypwr),max(Ypwr)]),
                                  title='Wavemeter Power', xaxis_title='', yaxis_title='Power (uW)',
                                  font=dict(color='white'),
@@ -107,25 +129,31 @@ def update_wavepower(n):
                                  plot_bgcolor='rgba(0,0,0,0)'
                     )}
 
+
 @app.callback(
     Output('live-graph-3', 'figure'),
-    [ Input('graph-update-3', 'n_intervals') ]
+    [Input('graph-update-3', 'n_intervals')]
 )
 def update_trace(n):
-    Xpwr.append(Xwlen[-1]+1)
-    Ypwr.append(wlm.powers[0])
+    # oscope.setup_trace(channel='CHAN2', time_scale=0.05, volt_scale=0.08, pos=0)
+    trace = oscope.get_trace(channel='CHAN2', plotting=False)
+
+    # Xpwr.append(Xwlen[-1]+1)
+    # Ypwr.append(wlm.powers[0])
+    Xtrace = np.linspace(0, len(trace), len(trace))
+    Ytrace = trace
 
     data = plotly.graph_objs.Scatter(
-            x=list(Xpwr),
-            y=list(Ypwr),
+            x=list(Xtrace),
+            y=list(Ytrace),
             name='Scatter',
-            mode= 'lines+markers'
+            mode='lines+markers'
     )
 
     return {'data': [data],
-            'layout' : go.Layout(xaxis=dict(range=[min(Xpwr),max(Xpwr)]),
-                                 yaxis=dict(range=[min(Ypwr),max(Ypwr)]),
-                                 title='Wavemeter Power', xaxis_title='', yaxis_title='Power (uW)',
+            'layout': go.Layout(xaxis=dict(range=[min(Xtrace),max(Xtrace)]),
+                                 yaxis=dict(range=[min(Ytrace),max(Ytrace)]),
+                                 title='Oscilloscope', xaxis_title='', yaxis_title='Voltage (V)',
                                  font=dict(color='white'),
                                  paper_bgcolor='rgba(0,0,0,0)',
                                  plot_bgcolor='rgba(0,0,0,0)'
