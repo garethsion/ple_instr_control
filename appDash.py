6c86867cc0c1626b12fc5e256633d8ccea989e18
garethsion <garethsion@googlemail.com>
2022-09-04 16:14:56 +0100

    Tidyed things up a bit

--- appDash.py
+++ appDash.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import dash
 from dash.dependencies import Output, Input
 import dash_core_components as dcc
@@ -15,6 +16,10 @@ import time
 from src.high_finesse_ws6.src.high_finesse import WavelengthMeter
 from src.Oscope import Oscope
 
+###########################
+#### SETUP INSTRUMENTS ####
+###########################
+
 # Connect the wavemeter
 wlm = WavelengthMeter(dllpath="C:\\Program Files (x86)\\HighFinesse\\Wavelength Meter WS Ultimate 1653\\Projects\\64\\wlmData.dll")
 
@@ -25,64 +30,54 @@ optstr = "AddTermCharToWriteBinBLock=True, TerminationCharacter='\n',AssureWrite
 oscope = Oscope(resource_string, optstr)
 # oscope.setup_trace(channel='CHAN2', time_scale=0.05, volt_scale=0.08, pos=0)
 
+###################################
+#### DECLARE INITIAL VARIABLES ####
+###################################
 Xwlen = deque(maxlen=600)
 Xwlen.append(1)
-
 Ywlen = deque(maxlen=600)
 Ywlen.append(wlm.wavelengths[0])
 
 Xpwr = deque(maxlen=600)
 Xpwr.append(1)
-
 Ypwr = deque(maxlen=600)
 Ypwr.append(wlm.powers[0])
 
-oscope.setup_trace(channel='CHAN2', time_scale=0.05, volt_scale=0.08, pos=0)
-# trace = oscope.get_trace(channel='CHAN2', plotting=False)
-
-# Xtrace = np.linspace(0,len(trace), len(trace))
-# Ytrace = trace
+###################
+#### SETUP APP ####
+###################
 
 cache = diskcache.Cache('./cache')
 lcm = DiskcacheLongCallbackManager(cache)
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG], long_callback_manager=lcm)
 
-app.layout = html.Div(
-    [
-        dbc.Row(html.Div(html.H1('Laser Stabilisation Control', style={'textAlign': 'center'}))),
-        dbc.Row(
-            [
-            dbc.Col(html.Div([
-            dcc.Graph(id = 'live-graph-1',
-                  animate = False),
-            dcc.Interval(
-                id='graph-update-1',
-                interval=20,
-                n_intervals=0
-            )]
-        ), width=8),
-            dbc.Col(html.Div(html.H5('Hello')))
-            ]),
-        dbc.Row([
-            dbc.Col(html.Div([
-                dcc.Graph(id='live-graph-2',
-                    animate=False),
-                dcc.Interval(
-                    id='graph-update-2',
-                    interval=20,
-                    n_intervals=0
-            )]), width=4),
-            dbc.Col(html.Div([
-                dcc.Graph(id='live-graph-3',
-                    animate=False),
-                dcc.Interval(
-                    id='graph-update-3',
-                    interval=6000,
-                    n_intervals=0
-            )]), width=4)
+####################
+#### APP LAYOUT ####
+####################
+
+Row1 = dbc.Row(html.Div(html.H1('Laser Stabilisation Control', style={'textAlign': 'center'})))
+Row2 = dbc.Row([dbc.Col(html.Div([dcc.Graph(id='live-graph-1', animate=False),
+                                  dcc.Interval(id='graph-update-1', interval=20, n_intervals=0),
+                                  dcc.Store(id='store-wlen')]), width=8),
+                dbc.Col(html.Div(html.H5('Hello')))
+                # dbc.Col(html.Div(dcc.Graph(id='fit-graph', animate='False')))
         ])
-    ]
+Row3 = dbc.Row([dbc.Col(html.Div([dcc.Graph(id='live-graph-2', animate=False),
+                                  dcc.Interval(id='graph-update-2', interval=20, n_intervals=0)]), width=4),
+                dbc.Col(html.Div([dcc.Graph(id='live-graph-3', animate=False),
+                                  dcc.Interval(id='graph-update-3', interval=6000, n_intervals=0)]), width=4)
+        ])
+
+container = [Row1, Row2, Row3]
+
+app.layout = html.Div(
+    container
 )
+
+#######################
+#### APP CALLBACKS ####
+#######################
+
 
 @app.callback(
     Output('live-graph-1', 'figure'),
@@ -100,13 +95,26 @@ def update_wavemeter(n):
     )
 
     return {'data': [data],
-            'layout': go.Layout(xaxis=dict(range=[min(Xwlen),max(Xwlen)]),
-                                 yaxis=dict(range=[min(Ywlen),max(Ywlen)]),
+            'layout': go.Layout(xaxis=dict(range=[min(Xwlen), max(Xwlen)]),
+                                 yaxis=dict(range=[min(Ywlen), max(Ywlen)]),
                                  title='Wavemeter', xaxis_title='', yaxis_title='Wavelength (nm)',
                                  font=dict(color='white'),
                                  paper_bgcolor='rgba(0,0,0,0)',
                                  plot_bgcolor='rgba(0,0,0,0)'
                     )}
+
+
+@app.callback(
+    Output('store-wlen', 'data'),
+    [Input('graph-input-2', 'n_intervals')],
+)
+def store_wavemeter(n):
+    Xwlen.append(Xwlen[-1]+1)
+    Ywlen.append(wlm.wavelengths[0])
+
+    data = pd.DataFrame(data={'x': Xwlen, 'y': Ywlen})
+
+    return data.to_json()
 
 
 @app.callback(
@@ -162,6 +170,35 @@ def update_trace(n):
                                  paper_bgcolor='rgba(0,0,0,0)',
                                  plot_bgcolor='rgba(0,0,0,0)'
                     )}
+
+
+@app.callback(
+    Output('fit-graph', 'figure'),
+    [Input('store-wlen', 'data') ]
+)
+def plot_fit(json_data):
+    df = pd.read_json(json_data)
+    print(df)
+    return
+    # Xpwr.append(Xwlen[-1]+1)
+    # Ypwr.append(wlm.powers[0])
+    #
+    # data = plotly.graph_objs.Scatter(
+    #         x=list(Xpwr),
+    #         y=list(Ypwr),
+    #         name='Scatter',
+    #         mode='lines+markers'
+    # )
+    #
+    # return {'data': [data],
+    #         'layout': go.Layout(xaxis=dict(range=[min(Xpwr),max(Xpwr)]),
+    #                              yaxis=dict(range=[min(Ypwr),max(Ypwr)]),
+    #                              title='Wavemeter Power', xaxis_title='', yaxis_title='Power (uW)',
+    #                              font=dict(color='white'),
+    #                              paper_bgcolor='rgba(0,0,0,0)',
+    #                              plot_bgcolor='rgba(0,0,0,0)'
+    #                 )}
+
 
 if __name__ == '__main__':
     app.run_server()
